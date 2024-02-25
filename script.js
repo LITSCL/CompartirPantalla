176d2302299a2c4427d93cd7946ac3239d2ff1c3
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-02-25 19:34:10 -0300

    Ok

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ const button_iniciar = document.getElementById("iniciar");
 const button_detener = document.getElementById("detener");
 const video_video = document.getElementById("video");
 
-// Configuración para la obtención del video de la pantalla.
+//Configuración para la obtención del video de la pantalla.
 var displayMediaOptions = {
-  video: {},
-  audio: true
+    video: {},
+    audio: true
 };
 
 //Listener para el botón "Iniciar".
 button_iniciar.addEventListener("click", function() {
-  iniciarCaptura();
+    iniciarCaptura();
 });
 
 //Listener para el botón "Detener".
 button_detener.addEventListener("click", function() {
-  detenerCaptura();
+    detenerCaptura();
 });
 
 async function iniciarCaptura() {
-  try {
-    video_video.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-  } catch(error) {
-    console.log("Error: " + error);
-  }
+    try {
+        video_video.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    } catch(error) {
+        console.log("Error: " + error);
+    }
 }
 
 function detenerCaptura() {
-  let pistas = video_video.srcObject.getTracks();
+    let pistas = video_video.srcObject.getTracks();
 
-  pistas.forEach(track => track.stop());
-  video_video.srcObject = null;
+    pistas.forEach(track => track.stop());
+    video_video.srcObject = null;
 }
